8328919c114ce20eacf8983e6f934c0c2a06c2eb
eder <ederarboleda22@gmail.com>
2024-10-29 23:40:56 -0500

    variables decimales

--- ejercicio1.js
+++ ejercicio1.js
@@ -23,3 +23,15 @@ console.log("La suma es:", suma);
 
 let resta = numero1 - numero2;
 console.log("La resta es:", resta); 
+
+
+// numeros decimales
+
+let numero3 = 10.4;
+let numero4 = 6.6;
+
+let multiplicacion = numero3 * numero4;
+console.log("La multiplicación es:", multiplicacion); 
+
+let division = numero3 / numero4;
+console.log("La división es:", division);
