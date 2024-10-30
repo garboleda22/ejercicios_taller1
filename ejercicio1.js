19f71cea3feb4e8666490f38ef2e4c690547d3e3
eder <ederarboleda22@gmail.com>
2024-10-29 23:35:26 -0500

    variable boleanos

--- ejercicio1.js
+++ ejercicio1.js
@@ -1,3 +1,13 @@
 //variable con string
 let pais = "Colombia"
 console.log(pais)
+
+//variable boleanos
+let dia = true;
+
+if (dia = true) {
+    console.log("Es de día");
+
+} else {
+    console.log("No es de noche");
+}
