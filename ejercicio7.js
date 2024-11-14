43bbe79adac30f3262d5a38277fc2536b2164038
garboleda22 <ederarboleda22@gmail.com>
2024-11-13 20:28:02 -0500

    avance_ejer7

--- ejercicio7.js
+++ ejercicio7.js
@@ -1,7 +1,41 @@
-class linkedlinst {
+export class nodo {
     constructor (value) {
         this.value = value;
         this.next = null;
     }
 }
 
+export class listaSimple {
+    constructor() {
+        this.head = null;
+
+    }
+    insertNode(value){
+        const newNode = new nodo(value);
+        if (this.head){
+        }
+        let currentNode = this.head;
+        while (currentNode.next) {
+            currentNode = currentNode.next;
+        }
+        currentNode.next = newNode;
+    }
+    displayList (value) {
+        let currentNode = this.head;
+        while (currentNode) {
+            console.log(currentNode.value);
+            currentNode = this.next;
+        }
+    }
+    findValue (value){
+        let currentNode = this.head;
+        while (currentNode) {
+            if (currentNode.value === value) {
+                return currentNode;
+            }
+            currentNode = currentNode.next;
+        }
+        return "NO SE ENCONTRO ELEMENTO";
+    }
+    
+}
